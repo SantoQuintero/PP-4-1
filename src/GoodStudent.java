2ba2205d9c2c7afe91413399117223fd048569c2
Santo Quintero <91106980@mvusd.net>
2022-12-15 09:07:31 -0800

    Initial commit

---
+++ src/GoodStudent.java
@@ -0,0 +1,150 @@
+public class GoodStudent
+{
+    private String firstName;
+    private String lastName;
+    private int id;
+    private double gpa;
+    private boolean isCsStudent;
+
+    //creates a student object fully
+    public GoodStudent(String name, int id, double gpa, String isCsStudent)
+    {
+        firstName = name.substring(0,name.indexOf(" "));
+        lastName = name.substring(name.indexOf(" ")+1);
+        this.id = id;
+        this.gpa = gpa;
+        if(isCsStudent.equals("Yes"))
+        {
+            this.isCsStudent = true;
+        }
+        else
+        {
+            this.isCsStudent = false;
+        }
+    }//end of constructor full
+
+    //creates a student object partially
+    public GoodStudent(String name, String isCsStudent)
+    {
+        firstName = name.substring(0,name.indexOf(" "));
+        lastName = name.substring(name.indexOf(" ")+1);
+        id = -1;
+        gpa = -1;
+        if(isCsStudent.equals("Yes"))
+        {
+            this.isCsStudent = true;
+        }
+        else
+        {
+            this.isCsStudent = false;
+        }
+    }//end of constructor partially
+
+
+    //creates a student object empty
+    public GoodStudent()
+    {
+        firstName = "";
+        lastName = "";
+        id = -1;
+        gpa = -1;
+        isCsStudent = false;
+    }//end of constructor empty
+
+    //returns firstName
+    public String getFirstName()
+    {
+        return firstName;
+    }//end of getFirstName
+
+    //returns lastName
+    public String getLastName()
+    {
+        return lastName;
+    }//end of getLastName
+
+
+    //returns id
+    public int getId()
+    {
+        return id;
+    }//end of getId
+
+    //returns gpa
+    public double getGpa()
+    {
+        return gpa;
+    }//end of getGpa
+
+    //returns isCsStudent
+    public boolean getIsCsStudent()
+    {
+        return isCsStudent;
+    }//end of getIsCsStudent
+
+    //sets firstName
+    public void setName(String firstName)
+    {
+        this.firstName = firstName;
+    }//end of setFirstName
+
+    //sets lastName
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }//end of setLastName
+
+    //sets id
+    public void setId(int id)
+    {
+        this.id = id;
+    }//end of setId
+
+    //sets gpa
+    public void setGpa(double gpa)
+    {
+        this.gpa = gpa;
+    }//end of setGpa
+
+    //sets isCsStudent
+    public void setIsCsStudent(boolean isCsStudent)
+    {
+        this.isCsStudent = isCsStudent;
+    }//end of setIsCsStudent
+
+    //returns a created id
+    public String createId()
+    {
+        return firstName.substring(0,1).toLowerCase()+lastName.toLowerCase()+id%1000;
+    }//end of createId
+
+    //returns the boosted gpa
+    public double getCsGpa()
+    {
+        if(isCsStudent)
+        {
+            return gpa*1.15;
+        }
+        else
+        {
+            return gpa;
+        }
+    }//end of csGpa
+
+
+
+
+
+
+
+
+    //returns the student information in a string
+    public String toString()
+    {
+
+        return "First Name: " + firstName + "\nLast Name: " + lastName + "\nID: " + id + "\nRaw GPA: " + gpa + "\nCS Student: " + isCsStudent;
+    }//end of toString
+
+}//end of class
+
+
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        GoodStudent student1= new GoodStudent("Alan Turing", 191254, 3.56, "Yes");
+        GoodStudent student2= new GoodStudent("Florence Nightingale", 182010, 3.89, "No");
+        GoodStudent student3= new GoodStudent("Grace Hopper", 190692, 3.99, "Yes");
+        System.out.println(student1.toString());
+        student2.setId(95366);
+        student2.setGpa(3.92);
+        System.out.println("Florence’s ID: " + student2.getId() + " and gpa: " + student2.getGpa());
+        System.out.println("Student 3’s first name: " + student3.getFirstName()+  " Enrolled in CS: " + student3.getIsCsStudent());
+        System.out.println("Student 1’s (Alan Turing) new ID: " + student1.createId());
+        System.out.println("Student 2’s (Florence Nightingale) new ID: " + student2.createId());
+        System.out.println("Student 3’s (Grace Hopper) new ID: " + student3.createId());
+        System.out.println("Student 1’s (Alan Turing) Computer Science GPA: " + student1.getCsGpa());
+        System.out.println("Student 2’s (Florence Nightingale) Computer Science GPA: " + student2.getCsGpa());
+        System.out.println("Student 3’s (Grace Hopper) Computer Science GPA: " + student3.getCsGpa());
+
+    }//end of main
+}//end of class
